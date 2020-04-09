83b7388c19dfa5991ca4b95b36485579cc6f003f
Vlad Onose <vladonose@yahoo.com>
2020-04-09 13:37:06 +0300

    C.1

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
@@ -2,10 +2,12 @@ package ro.pub.cs.systems.eim.Colocviu1_245;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Colocviu1_245MainActivity extends AppCompatActivity {
 
@@ -36,5 +38,20 @@ public class Colocviu1_245MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        computeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Colocviu1_245SecondaryActivity.class);
+                intent.putExtra(Constants.COMPUTE_ME, allTerms.getText().toString());
+                startActivityForResult(intent, Constants.SECONDARY_REQ_CODE);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == Constants.SECONDARY_REQ_CODE) {
+            Toast.makeText(this, "Computed value is: " + resultCode, Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Constants.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Constants.java
@@ -3,4 +3,6 @@ package ro.pub.cs.systems.eim.Colocviu1_245;
 public class Constants {
     public static String COMPUTE_ME = "compute";
     public static String ADD_SIGN = "+";
+
+    public static int SECONDARY_REQ_CODE = 1;
 }
